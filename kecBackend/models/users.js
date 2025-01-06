76cf09b546bf848fd1574224930284a86d92394f
Safrinakbar <safrinbarganaa.22cse@kongu.edu>
2025-01-06 13:33:10 +0530

    commit

--- kecBackend/models/users.js
+++
@@ -1,9 +0,0 @@
-var mdb= require('mongoose')
-
-var userSchema = mdb.Schema({
-    email:String,
-    password:String
-})
-
-var user_schema = mdb.model("users",userSchema)
-module.exports = user_schema
